ca26760600c52b690d28d7641c2d0bf645367a22
Bruno Waschburger Silva <bwaschburger@gmail.com>
2025-04-27 20:48:13 -0300

    Iniciei o projeto fazendo a matriz.

---
+++ src/jogar/BatalhaNaval.java
@@ -0,0 +1,27 @@
+package jogar;
+
+import java.util.Scanner;
+import java.util.Random;
+
+public class BatalhaNaval {
+    public static void main(String[] args) {
+        Scanner ler = new Scanner(System.in);
+        Random aleatorio = new Random();
+        int[][] tabela = new int[10][10];
+
+        System.out.print("   ");
+        for (char coluna = 'A'; coluna <= 'J'; coluna++) {
+            System.out.printf("%-3s", coluna);
+        }
+        System.out.println();
+
+        for (int l = 0; l < tabela.length; l++) {
+            System.out.printf("%-3d", l);
+            for (int c = 0; c < tabela[0].length; c++) {
+                tabela[l][c] = aleatorio.nextInt(1, 100);
+                System.out.printf("%-3s", "*");
+            }
+            System.out.println();
+        }
+    }
+}
